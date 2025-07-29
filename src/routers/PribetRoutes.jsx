04105933e6d87fe8b6888dab815ceb51b5f66a03
Anondoraydev <Anondo554@gmail.com>
2025-07-29 22:20:06 +0600

    'update'

--- src/routers/PribetRoutes.jsx
+++ src/routers/PribetRoutes.jsx
@@ -1,8 +1,8 @@
 import PropTypes from "prop-types";
 import { useContext } from "react";
-import { AuthContext } from "../context/AuthProvider";
 import { Navigate } from "react-router-dom";
 import Loading from "../components/Loading/Loading";
+import { AuthContext } from "../context/AuthProvider";
 
 const PribetRoutes = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
@@ -10,11 +10,12 @@ const PribetRoutes = ({ children }) => {
     return <Loading />;
   } else if (!user) {
     return <Navigate to="/login" />;
-  } 
+  }
   return children;
 };
 
 PribetRoutes.propTypes = {
+  
   children: PropTypes.node.isRequired,
 };
 
